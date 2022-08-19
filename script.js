1505f68bd184161ca7883369286082de0285124b
Hanbi58 <hanxiaonan2012@gmail.com>
2022-08-18 20:05:20 -0700

    WIP

--- script.js
+++ script.js
@@ -3,4 +3,33 @@ const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
 
-const activeAmount = 1;
+let activeAmount = 1;
+
+next.addEventListener("click", () => {
+  activeAmount++;
+  activeAmount = Math.min(activeAmount++, circles.length);
+  console.log(activeAmount);
+  update();
+});
+
+prev.addEventListener("click", () => {
+  activeAmount--;
+  activeAmount = Math.max(activeAmount++, 1);
+  console.log(activeAmount);
+  update();
+});
+
+function update() {
+  if (activeAmount === 1) {
+    prev.disabled = true;
+  } else if (activeAmount === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+
+  console.log(activeAmount / circles.length);
+  progress.style.width =
+    ((activeAmount - 1) / (circles.length - 1)) * 100 + "%";
+}
